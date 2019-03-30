7667b745985d71b547fcc2024a4b920f43ecb42e
CDulouard <clement.dulouard@gmail.com>
2019-03-30 18:03:56 +0100

    ajout d'un menu principal basique

---
+++ ProrokUnitTest2V3/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private static int _sceneToLoad;
+
+    private void Start()
+    {
+        _sceneToLoad = 0;
+    }
+
+    public void SceneSwitcher()
+    {
+        SceneManager.LoadScene(_sceneToLoad);
+    }
+
+    public void SelectScene(int nb)
+    {
+        _sceneToLoad = nb;
+    }
+    
+}

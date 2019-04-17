88e5e06866417070958d006d64b8b09e544b53aa
CDulouard <clement.dulouard@gmail.com>
2019-04-17 13:50:37 +0200

    ajout de champs texte editables dans les settings

--- ProrokUnitTest2V3/Assets/Scripts/UIScripts/BoxManager.cs
+++ ProrokUnitTest2V3/Assets/Scripts/UIScripts/BoxManager.cs
@@ -7,55 +7,18 @@ namespace UIScripts
     {
         public void HideContent(GameObject box)
         {
-            /*
-            var images = box.GetComponentsInChildren<Image>();
-            foreach (var child in images)
-            {
-                child.enabled = false;
-            }
-
-            var texts = box.GetComponentsInChildren<Text>();
-            foreach (var child in texts)
-            {
-                child.enabled = false;
-            }*/
             box.SetActive(false);
 
         }
         
         public void ShowContent(GameObject box)
         {
-            /*
-            var images = box.GetComponentsInChildren<Image>();
-            foreach (var child in images)
-            {
-                child.enabled = true;
-            }
-
-            var texts = box.GetComponentsInChildren<Text>();
-            foreach (var child in texts)
-            {
-                child.enabled = true;
-            }*/
-            /*==============================*/
             box.SetActive(true);
     
         }
         
         public static void HideContentGeneric(GameObject box)
         {
-            /*
-            var images = box.GetComponentsInChildren<Image>();
-            foreach (var child in images)
-            {
-                child.enabled = false;
-            }
-
-            var texts = box.GetComponentsInChildren<Text>();
-            foreach (var child in texts)
-            {
-                child.enabled = false;
-            }*/
             box.SetActive(false);
             
         }
--- ProrokUnitTest2V3/Assets/Scripts/UIScripts/SettingsManager.cs
+++ ProrokUnitTest2V3/Assets/Scripts/UIScripts/SettingsManager.cs
@@ -6,13 +6,11 @@ namespace UIScripts
 {
     public class SettingsManager : MonoBehaviour
     {
-
-        
-        public Text camViewDistanceText;
-        public Text xSensivityText;
-        public Text ySensivityText;
-        public Text scrollSensivityText;
-        public Text cameraDistanceText;
+        public InputField camViewDistanceField;
+        public InputField xSensivityField;
+        public InputField ySensivityField;
+        public InputField scrollSensivityField;
+        public InputField cameraDistanceField;
 
         public Scrollbar camViewDistanceScroll;
         public Scrollbar xSensivityScroll;
@@ -39,16 +37,21 @@ namespace UIScripts
 
         private void Start()
         {
+            /*    Load actual settings    */
             settings = Settings.FromJson(Application.streamingAssetsPath + "/JsonFiles/settings.json");
-            
-            camViewDistanceText.text = ((int) settings.cameraViewDistance).ToString(CultureInfo.InvariantCulture);
-            xSensivityText.text = ((int) settings.xSensivity).ToString(CultureInfo.InvariantCulture);
-            ySensivityText.text = ((int) settings.ySensivity).ToString(CultureInfo.InvariantCulture);
-            scrollSensivityText.text = ((int) settings.scrollSensivity).ToString(CultureInfo.InvariantCulture);
-            cameraDistanceText.text = ((int) settings.cameraDefaultDistance).ToString(CultureInfo.InvariantCulture);
+
+            /*    Write settings values to the UI    */
+            camViewDistanceField.text = ((int) settings.cameraViewDistance).ToString(CultureInfo.InvariantCulture);
+            xSensivityField.text = ((int) settings.xSensivity).ToString(CultureInfo.InvariantCulture);
+            ySensivityField.text = ((int) settings.ySensivity).ToString(CultureInfo.InvariantCulture);
+            scrollSensivityField.text = ((int) settings.scrollSensivity).ToString(CultureInfo.InvariantCulture);
+            cameraDistanceField.text = ((int) settings.cameraDefaultDistance).ToString(CultureInfo.InvariantCulture);
 
             // x = (v * (max - min)) + min
             //    (x - min) / (max - min) = v
+
+            /*    Convert the value of the settings to a value for scroll bars    */
+
             camViewDistanceScroll.value = (settings.cameraViewDistance - CamViewDistanceMin) /
                                           (CamViewDistanceMax - CamViewDistanceMin);
 
@@ -68,27 +71,171 @@ namespace UIScripts
 
         private void Update()
         {
+            /*    Convert the value of the input fields to a value for scroll bars or  apply the value of the scroll bar to the UI   */
+
+            if (float.TryParse(camViewDistanceField.text, out var camViewDistanceFieldValue) && camViewDistanceField.isFocused)
+            {
+                if (camViewDistanceFieldValue > CamViewDistanceMax)
+                {
+                    if (!camViewDistanceField.isFocused)
+                    {
+                        camViewDistanceField.text = ((int) CamViewDistanceMax).ToString(CultureInfo.InvariantCulture);
+                    }
+
+                    camViewDistanceScroll.value = 1;
+                }
+                else if (camViewDistanceFieldValue < CamDistanceMin)
+                {
+                    if (!camViewDistanceField.isFocused)
+                    {
+                        camViewDistanceField.text = ((int) CamDistanceMin).ToString(CultureInfo.InvariantCulture);
+                    }
+                    camViewDistanceScroll.value = 0;
+                }
+                else
+                {
+                    camViewDistanceScroll.value = (camViewDistanceFieldValue - CamViewDistanceMin) /
+                                                  (CamViewDistanceMax - CamViewDistanceMin);
+                }
+            }
+            else
+            {
+                camViewDistanceField.text = ((int) settings.cameraViewDistance).ToString(CultureInfo.InvariantCulture);
+            }
+            
+            if (float.TryParse(xSensivityField.text, out var xSensivityFieldValue) && xSensivityField.isFocused)
+            {
+                if (xSensivityFieldValue > XSensivityMax)
+                {
+                    if (!xSensivityField.isFocused)
+                    {
+                        xSensivityField.text = ((int) XSensivityMax).ToString(CultureInfo.InvariantCulture);
+                    }
+
+                    xSensivityScroll.value = 1;
+                }
+                else if (xSensivityFieldValue < XSensivityMin)
+                {
+                    if (!xSensivityField.isFocused)
+                    {
+                        xSensivityField.text = ((int) XSensivityMin).ToString(CultureInfo.InvariantCulture);
+                    }
+                    xSensivityScroll.value = 0;
+                }
+                else
+                {
+                    xSensivityScroll.value = (xSensivityFieldValue - XSensivityMin) /
+                                                  (XSensivityMax - XSensivityMin);
+                }
+            }
+            else
+            {
+                xSensivityField.text = ((int) settings.xSensivity).ToString(CultureInfo.InvariantCulture);
+            }
+            
+            if (float.TryParse(ySensivityField.text, out var ySensivityFieldValue) && ySensivityField.isFocused)
+            {
+                if (ySensivityFieldValue > YSensivityMax)
+                {
+                    if (!ySensivityField.isFocused)
+                    {
+                        ySensivityField.text = ((int) YSensivityMax).ToString(CultureInfo.InvariantCulture);
+                    }
+
+                    ySensivityScroll.value = 1;
+                }
+                else if (ySensivityFieldValue < YSensivityMin)
+                {
+                    if (!ySensivityField.isFocused)
+                    {
+                        ySensivityField.text = ((int) YSensivityMin).ToString(CultureInfo.InvariantCulture);
+                    }
+                    ySensivityScroll.value = 0;
+                }
+                else
+                {
+                    ySensivityScroll.value = (ySensivityFieldValue - YSensivityMin) /
+                                             (YSensivityMax - YSensivityMin);
+                }
+            }
+            else
+            {
+                ySensivityField.text = ((int) settings.ySensivity).ToString(CultureInfo.InvariantCulture);
+            }
+            
+            if (float.TryParse(scrollSensivityField.text, out var scrollSensivityFieldValue) && scrollSensivityField.isFocused)
+            {
+                if (scrollSensivityFieldValue > ScrollSensivityMax)
+                {
+                    if (!scrollSensivityField.isFocused)
+                    {
+                        scrollSensivityField.text = ((int) ScrollSensivityMax).ToString(CultureInfo.InvariantCulture);
+                    }
+
+                    scrollSensivityScroll.value = 1;
+                }
+                else if (scrollSensivityFieldValue < ScrollSensivityMin)
+                {
+                    if (!scrollSensivityField.isFocused)
+                    {
+                        scrollSensivityField.text = ((int) ScrollSensivityMin).ToString(CultureInfo.InvariantCulture);
+                    }
+                    scrollSensivityScroll.value = 0;
+                }
+                else
+                {
+                    scrollSensivityScroll.value = (scrollSensivityFieldValue - ScrollSensivityMin) /
+                                             (ScrollSensivityMax - ScrollSensivityMin);
+                }
+            }
+            else
+            {
+                scrollSensivityField.text = ((int) settings.scrollSensivity).ToString(CultureInfo.InvariantCulture);
+            }
+
+            if (float.TryParse(cameraDistanceField.text, out var cameraDistanceFieldValue) && cameraDistanceField.isFocused)
+            {
+                if (cameraDistanceFieldValue > CamDistanceMax)
+                {
+                    if (!cameraDistanceField.isFocused)
+                    {
+                        cameraDistanceField.text = ((int) CamDistanceMax).ToString(CultureInfo.InvariantCulture);
+                    }
+
+                    cameraDistanceScroll.value = 1;
+                }
+                else if (cameraDistanceFieldValue < CamDistanceMin)
+                {
+                    if (!cameraDistanceField.isFocused)
+                    {
+                        cameraDistanceField.text = ((int) CamDistanceMin).ToString(CultureInfo.InvariantCulture);
+                    }
+                    cameraDistanceScroll.value = 0;
+                }
+                else
+                {
+                    cameraDistanceScroll.value = (cameraDistanceFieldValue - CamDistanceMin) /
+                                                  (CamDistanceMax - CamDistanceMin);
+                }
+            }
+            else
+            {
+                cameraDistanceField.text = ((int) settings.cameraDefaultDistance).ToString(CultureInfo.InvariantCulture);
+            }
+            /*    Convert the value of scroll bars to setting values    */
             settings.cameraViewDistance = camViewDistanceScroll.value * (CamViewDistanceMax - CamViewDistanceMin) +
-                                           CamViewDistanceMin;
+                                          CamViewDistanceMin;
 
             settings.xSensivity = xSensivityScroll.value * (XSensivityMax - XSensivityMin) + XSensivityMin;
 
             settings.ySensivity = ySensivityScroll.value * (YSensivityMax - YSensivityMin) + YSensivityMin;
 
             settings.scrollSensivity = scrollSensivityScroll.value * (ScrollSensivityMax - ScrollSensivityMin) +
-                                        ScrollSensivityMin;
-            
+                                       ScrollSensivityMin;
+
             settings.cameraDefaultDistance = cameraDistanceScroll.value * (CamDistanceMax - CamDistanceMin) +
-                                        CamDistanceMin;
+                                             CamDistanceMin;
         }
 
-        private void LateUpdate()
-        {
-            camViewDistanceText.text = ((int) settings.cameraViewDistance).ToString(CultureInfo.InvariantCulture);
-            xSensivityText.text = ((int) settings.xSensivity).ToString(CultureInfo.InvariantCulture);
-            ySensivityText.text = ((int) settings.ySensivity).ToString(CultureInfo.InvariantCulture);
-            scrollSensivityText.text = ((int) settings.scrollSensivity).ToString(CultureInfo.InvariantCulture);
-            cameraDistanceText.text = ((int) settings.cameraDefaultDistance).ToString(CultureInfo.InvariantCulture);
-        }
     }
 }
